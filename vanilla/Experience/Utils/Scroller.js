134229649673ceb1d6cadae6f88e90dfd42c4dc8
Elan Bartholomew <elawn@me.com>
2022-11-29 19:03:30 -0700

    added a scrollbox

--- vanilla/Experience/Utils/Scroller.js
+++ vanilla/Experience/Utils/Scroller.js
@@ -6,16 +6,40 @@ export default class Scroller extends EventEmitter {
         super()
         this.exp = new Experience()
         this.currentSect = 0
+        this.maxSect = document.querySelectorAll( 'section' ).length - 1
+        this.scrollBox = document.querySelector( '#scrollbox' )
+        this.killBox = false
+        this.showTimeout = setTimeout( () => {
+            this.showBox = true
+        }, 8000 )
 
         this.handleScroll()
     }
 
+    set showBox( val ) {
+        this.scrollBox.classList.toggle( 'active', val )
+    }
+
+    get showBox() {
+        return this.scrollBox.classList.contains( 'active' )
+    }
+
     handleScroll() {
         window.addEventListener( 'scroll', () => {
+            this.showBox = false
+            clearTimeout( this.showTimeout )
+            if ( !this.killBox ) {
+                this.showTimeout = setTimeout( () => {
+                    this.showBox = true
+                }, 5000 )
+            }
             const newSect = Math.round( window.scrollY / this.exp.sizes.height )
             if ( newSect !== this.currentSect ) {
                 this.currentSect = newSect
                 this.trigger( 'newSect', [ this.currentSect ] )
+                if ( !this.killBox ) {
+                    this.killBox = ( newSect === this.maxSect )
+                }
             }
         } )
     }
