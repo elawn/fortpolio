8cd01a6ee99f4d567a3fffbae87f26ebe87b33a4
Elan Bartholomew <elawn@me.com>
2022-12-08 12:43:56 -0700

    added scrolltrigger camera position animation

--- vanilla/Experience/Camera.js
+++ vanilla/Experience/Camera.js
@@ -1,21 +1,29 @@
+import { gsap } from 'gsap'
 import Experience from './Experience'
 import { PerspectiveCamera } from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 export default class Camera {
     constructor() {
+        this.debug = false
+
         this.exp = new Experience()
         this.sizes = this.exp.sizes
         this.scene = this.exp.scene
         this.cvs = this.exp.cvs
 
+        this.baseY = -1.92119
+
         this.gui = this.exp.gui
         this.guiFolder = null
-
         this.setInstance()
         this.setControls()
 
         this.setupDebug()
+    }
+
+    get currY () {
+        return ( -window.scrollY / this.exp.sizes.height * this.sizes.objsDist ) + this.baseY
     }
 
     resize() {
@@ -25,8 +33,8 @@ export default class Camera {
 
     setControls() {
         this.ctls = new OrbitControls( this.instance, this.cvs )
-        this.ctls.enabled = false
-        // this.ctls.enableDamping = true
+        this.ctls.enabled = this.debug
+        // this.ctls.enableDamping = this.debug
     }
 
     setupDebug() {
@@ -38,13 +46,37 @@ export default class Camera {
 
     setInstance() {
         this.instance = new PerspectiveCamera( 5, this.sizes.width / this.sizes.height, 0.1, 1000 )
-        this.instance.position.set( 0, 0, 16 )
+        this.instance.position.set(
+            -3.11345,
+            this.baseY,
+            15.57611 )
+        this.instance.lookAt( 0, this.currY, 0 )
         this.scene.add( this.instance )
+
+        gsap.to( this.instance.position, {
+            duration: 1,
+            ease: 'sine.inOut',
+            repeat: 3,
+            yoyo: true,
+            onUpdate: () => {
+                this.instance.lookAt( 0, this.currY - this.baseY, 0 )
+            },
+            scrollTrigger: {
+                trigger: '#sections',
+                scrub: 1,
+                end: 'bottom bottom'
+            },
+            x: 2,
+            z: 16.5
+        } )
     }
 
     update() {
-        // this.ctls.update()
-        this.instance.position.y = -window.scrollY / this.exp.sizes.height * this.sizes.objsDist
+        if ( this.debug ) {
+            this.ctls.update()
+        } else {
+            this.instance.position.y = this.currY
+        }
         if ( this.guiFolder ) this.guiFolder.controllers.forEach( ctl => ctl.updateDisplay() )
     }
 }
--- vanilla/main.js
+++ vanilla/main.js
@@ -1,4 +1,7 @@
 import './style.css'
 import Experience from './Experience/Experience'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
 
-const exp = new Experience(document.querySelector('#cvs'))
+gsap.registerPlugin( ScrollTrigger )
+const exp = new Experience( document.querySelector( '#cvs' ) )
